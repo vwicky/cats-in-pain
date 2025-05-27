7ce541c52f655494bfa6c960627da90538b93319
vwicky <viktir.zozulka@gmail.com>
2025-05-27 11:52:35 +0300

    react-router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import LandingPage from './pages/LandingPage';
+import ChatPage from './pages/ChatPage';
+
 
 function App() {
 
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ChatPage() {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      
+    </div>
+  )
+}
+
+export default ChatPage
